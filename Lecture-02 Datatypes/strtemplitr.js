c02ac32d4095c6385d6e0adaf1453467de44108f
budsayamasNithi <6406021620149@fitm.kmutnb.ac.th>
2022-12-21 10:17:37 +0700

    update DataTypes.js

---
+++ Lecture-02 Datatypes/strtemplitr.js
@@ -0,0 +1,12 @@
+console.log("\n******* Connatenation with template litera");
+
+let str1 = "javaScript";
+let str2 = "fun";
+
+console.log(`I am writing code in ${str1}`);
+console.log(`Formatting in ${str1} is ${str2}!`);
+
+console.log("\n******Expreeion in tamplate literals*****\n");
+
+let bool1 = true;
+console.log()
